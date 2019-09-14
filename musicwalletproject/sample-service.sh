337c6fa370462d52e4ed1baeb2cec5ec5f592bd0
root <root@ip-172-31-7-99.us-east-2.compute.internal>
2019-09-14 17:37:44 +0000

    dockrizing project

---
+++ musicwalletproject/sample-service.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/bin/python /root/MusicWallet/musicwalletproject/manage.py runserver 0.0.0.0:8000 --settings=musicwalletproject.settings.development
---
+++ sample-service.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/bin/python manage.py runserver 0.0.0.0:8000 --settings=musicwalletproject.settings.development
